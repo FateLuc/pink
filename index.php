bb90d592859faa540ce7b6026a4d1e85755586a4
Feger Luc <luc.feger@lernende.bfo-vs.ch>
2019-11-20 08:35:32 +0100

    Add html stub

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-
-echo "hallo";
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php
+    echo "<h1>hallo</h1>";
+    ?>
+</body>
+</html>
