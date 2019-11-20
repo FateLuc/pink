02d70541593575f38544b6131d865f8b2c3a382f
Feger Luc <luc.feger@lernende.bfo-vs.ch>
2019-11-20 10:50:47 +0100

    Add name input field

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
-$name = "Luc";
+$name ="";
+if(isset($_GET['name'])) {
+    $name = $_GET['name'];
+}
 ?>
 
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body> <?php echo "<h1>hallo " . $name . "</h1>"; ?> </body>
+<body> <?php echo "<h1>Hallo " . $name . "</h1>";
+?>
+<p1>
+    <?php
+        echo "Ihr Name:";
+    ?>
+</p1>
+    <form>
+        <input type="text" name="name">
+        <input type="submit">
+    </form>
+
+</body>
 </html>
